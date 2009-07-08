321911a9c75fb8a7fdc956ef5a2e35e6de3b74a4
David Larochelle <dlarochelle@dlarochelle-laptop.(none)>
2009-07-08 17:12:23 -0400

    Add sql file to create the table for adwords data.

---
+++ india_asn/ad_words_schema.sql
@@ -0,0 +1,16 @@
+create table adwords_country_data (
+ country      text,
+ country_code varchar(2),
+ site_name  text,
+ category   text,
+ composition_index   integer,
+ audience_reach float,
+ audience_unique_users integer,
+ country_unique_users integer,
+ country_page_views integer,
+ gcn_text boolean,
+gcn_images text ,
+gcn_videos text ,
+gcn_gadgets text,
+ gcn_impressions_per_day integer
+);

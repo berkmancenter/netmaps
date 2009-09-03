603becc49bd26479cced8c13385b8276d7743873
David Larochelle <dlarochelle@dlarochelle-laptop.(none)>
2009-09-03 12:28:10 -0400

    Move *_get_url routines into get_url_routines.php previously they were declared in both geo_map_home.php and country_detail.php.

--- india_asn/asn_web_graphs/geo_map_home.php
+++ india_asn/asn_web_graphs/geo_map_home.php
@@ -8,7 +8,8 @@
 
 global $alternate_css;
 $alternate_css='geo_map.css';
-include "./header.php"
+include "./header.php";
+include "./get_url_routines.php";
 ?>
 
 <?
@@ -34,99 +35,9 @@ include "./geo_map_scripts.php";
 /**
  *
  *
- * @param unknown $country_xml
- * @return unknown
- */
-function get_image_url_base($country_xml) {
-    $host = $_SERVER["HTTP_HOST"];
-    $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
-    //print_r($country_xml);
-    //print_r($country_xml["country_name"]);
-    $country_name = $country_xml['country_name'];
-    // $country_name = get_country_name_x($country_xml);
-    //print_r($country_name);
-    $country_svg_url = "http://$host$path/results/graphs/asn-" .($country_name);
-
-    return $country_svg_url;
-}
-
-
-/**
- *
- *
- * @param unknown $country_xml
- * @return unknown
- */
-function get_json_summary_url($country_xml) {
-    $host = $_SERVER["HTTP_HOST"];
-    $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
-    $country_code = $country_xml['country_code'];
-
-    $country_json_summary_url = "http://$host$path/country_json_summary.php/?cc=$country_code";
-    return $country_json_summary_url;
-}
-
-
-/**
- *
- *
- * @return unknown
- */
-function get_flash_url() {
-    $host = $_SERVER["HTTP_HOST"];
-    $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
-    $country_svg_url = "http://$host$path/flare_demo/asn_visualization.swf";
-
-    return $country_svg_url;
-}
-
-
-/**
- *
- *
- * @param unknown $country_xml
- * @return unknown
- */
-function get_country_svg_image_url($country_xml) {
-    $country_svg_url =  get_image_url_base($country_xml) . ".svg";
-
-    $country_svg_url = htmlentities($country_svg_url, ENT_QUOTES );
-    return $country_svg_url;
-}
-
-
-/**
- *
- *
- * @param unknown $country_xml
- * @return unknown
- */
-function get_country_png_image_url($country_xml) {
-    $country_svg_url =  get_image_url_base($country_xml) . ".png";
-    $country_svg_url = htmlentities($country_svg_url, ENT_QUOTES );
-    return $country_svg_url;
-}
-
-
-/**
- *
- *
- * @param unknown $country_xml
- * @return unknown
- */
-function get_country_graphml_image_url($country_xml) {
-    $country_svg_url =  get_image_url_base($country_xml) . ".graphml";
-    $country_svg_url = htmlentities($country_svg_url, ENT_QUOTES );
-    return $country_svg_url;
-}
-
-
-/**
- *
- *
  * @param unknown $country_code
  */
-function embed_flash_object($country_code) {
+function embed_flash_object_bak($country_code) {
     $xml_file_location = 'results/results.xml';
 
     $xml = new SimpleXMLElement(file_get_contents($xml_file_location));
@@ -241,7 +152,7 @@ Country compared to Country<br/>
 <td>
 <div id="foo">
 <div id='country_flash_object'>
-  <? embed_flash_object('CN'); ?>
+  <? embed_flash_object_bak('CN'); ?>
 </div>
 </div>
 </td>
---
+++ india_asn/asn_web_graphs/get_url_routines.php
@@ -0,0 +1,91 @@
+<?
+
+/**
+ *
+ *
+ * @param unknown $country_xml
+ * @return unknown
+ */
+function get_image_url_base($country_xml) {
+    $host = $_SERVER["HTTP_HOST"];
+    $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
+    //print_r($country_xml);
+    //print_r($country_xml["country_name"]);
+    $country_name = $country_xml['country_name'];
+    // $country_name = get_country_name_x($country_xml);
+    //print_r($country_name);
+    $country_svg_url = "http://$host$path/results/graphs/asn-" .($country_name);
+
+    return $country_svg_url;
+}
+
+
+/**
+ *
+ *
+ * @param unknown $country_xml
+ * @return unknown
+ */
+function get_json_summary_url($country_xml) {
+    $host = $_SERVER["HTTP_HOST"];
+    $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
+    $country_code = $country_xml['country_code'];
+
+    $country_json_summary_url = "http://$host$path/country_json_summary.php/?cc=$country_code";
+    return $country_json_summary_url;
+}
+
+
+/**
+ *
+ *
+ * @return unknown
+ */
+function get_flash_url() {
+    $host = $_SERVER["HTTP_HOST"];
+    $path = rtrim(dirname($_SERVER["PHP_SELF"]), "/\\");
+    $country_svg_url = "http://$host$path/flare_demo/asn_visualization.swf";
+
+    return $country_svg_url;
+}
+
+
+/**
+ *
+ *
+ * @param unknown $country_xml
+ * @return unknown
+ */
+function get_country_svg_image_url($country_xml) {
+    $country_svg_url =  get_image_url_base($country_xml) . ".svg";
+
+    $country_svg_url = htmlentities($country_svg_url, ENT_QUOTES );
+    return $country_svg_url;
+}
+
+
+/**
+ *
+ *
+ * @param unknown $country_xml
+ * @return unknown
+ */
+function get_country_png_image_url($country_xml) {
+    $country_svg_url =  get_image_url_base($country_xml) . ".png";
+    $country_svg_url = htmlentities($country_svg_url, ENT_QUOTES );
+    return $country_svg_url;
+}
+
+
+/**
+ *
+ *
+ * @param unknown $country_xml
+ * @return unknown
+ */
+function get_country_graphml_image_url($country_xml) {
+    $country_svg_url =  get_image_url_base($country_xml) . ".graphml";
+    $country_svg_url = htmlentities($country_svg_url, ENT_QUOTES );
+    return $country_svg_url;
+}
+?>

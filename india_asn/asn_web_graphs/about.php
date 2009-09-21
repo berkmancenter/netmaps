921a5e55f81a8d7571bd1c14dccc0b3f73d9b93b
David Larochelle <dlarochelle@dlarochelle-laptop.(none)>
2009-09-21 14:18:36 -0400

    include the methods and data links underneath the country list view  box ala mia's screenshot.  These pages should have the current text within a single wide grey box, like the ones on the front page but  across the width of the page.
    Refactor the about text into a single php file about_text.php that is included in two places.

---
+++ india_asn/asn_web_graphs/about.php
@@ -0,0 +1,27 @@
+<?
+/**
+ * raw_data.php
+ *
+ * @package default
+ */
+
+
+global $alternate_css;
+$alternate_css='geo_map.css';
+global $nav_index;
+$show_nav_index=0;
+include "./header.php";
+
+?>
+<table class="info_box">
+<tr>
+<td>
+<? include "about_text.php"; ?>
+</td>
+</tr>
+</table>
+
+<?
+include "footer_new.php";
+?>
+
---
+++ india_asn/asn_web_graphs/footer_new.php
@@ -0,0 +1,21 @@
+<?
+/**
+ * footer.php
+ *
+ * @package default
+ * @see country_complexity_results.php
+ * @see country_detail.php
+ * @see geo_map_home.php
+ * @see home.php
+ * @see ips_per_points_of_control_results.php
+ * @see methods.php
+ * @see raw_data.php
+ */
+
+
+?>
+<div class=footer>
+<a href="about.php">ABOUT PROJECT</a> / <a href="methods.php">RESEARCH METHODS</a> / <a href="raw_data.php">RAW DATA</a>
+</div>
+</body>
+</html>

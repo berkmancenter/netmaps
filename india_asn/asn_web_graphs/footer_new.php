3306a8932b4b34fa7febc5cb11604d8ceaac0e3c
David Larochelle <dlarochelle@dlarochelle-laptop.(none)>
2009-09-23 15:37:47 -0400

    on list of pages at the bottom, include a 'See Maps' as first entry that just links back to the home page.
    also make the site title text link back to the home page.

--- india_asn/asn_web_graphs/footer_new.php
+++ india_asn/asn_web_graphs/footer_new.php
@@ -15,7 +15,7 @@
 
 ?>
 <div class=footer>
-<a href="about.php">ABOUT PROJECT</a> / <a href="methods.php">RESEARCH METHODS</a> / <a href="raw_data.php">RAW DATA</a>
+<a href="geo_map_home.php">SEE MAPS</a> / <a href="about.php">ABOUT PROJECT</a> / <a href="methods.php">RESEARCH METHODS</a> / <a href="raw_data.php">RAW DATA</a>
 </div>
 </body>
 </html>
--- india_asn/asn_web_graphs/header.php
+++ india_asn/asn_web_graphs/header.php
@@ -176,5 +176,5 @@ if ($show_nav_index) {
 <br/>
 <br/>
     <? } ?>
-<h1>Mapping Local Internet Control [private draft - please don't share]</h1>
+<h1 class="page_title_heading"><a href="./geo_map_home.php">Mapping Local Internet Control [private draft - please don't share]</a></h1>
 <h2><span style="color:white">from the Berkman Center for Intenet &amp; Society at Harvard University</span></h2>

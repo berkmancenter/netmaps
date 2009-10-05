ace38e31bea836ec63bdfae09065699fc25b74f4
David Larochelle <dlarochelle@dlarochelle-laptop.(none)>
2009-10-05 17:10:46 -0400

    Add CSV dumping function.

--- india_asn/asn_web_graphs/xml_utils.php
+++ india_asn/asn_web_graphs/xml_utils.php
@@ -434,3 +434,71 @@ function country_xml_list_summary_table($countries_xml, $show_rank) {
 </table>
 <?
 }
+
+function csv_dump($countries_xml)
+{
+
+  $column_headings = array(#'country_rank' => 'Rank',
+        'country_code' => 'Code',
+        'country_name' => 'Country',
+        'total_ips' => 'Total IPs',
+        'total_asns'=> 'Total Autonomous Systems',
+        'points_of_control' => 'Points of Control',
+        'ips_per_points_of_control' => 'IPs Per Point of Control',
+        'complexity' => 'Complexity',
+        'ad_planner_country_top_sites' => 'top sites',
+        'ad_planner_top_sites_in_country' => 'top sites in country',
+        'ad_planner_top_sites_country_percent' => 'country site %',
+        'ad_planner_top_sites_in_PoC' => 'top sites in PoC',
+        'ad_planner_top_sites_PoC_percent' => 'PoC sites %',
+        'ad_planner_total_page_views' => 'total page views',
+        'ad_planner_page_views_in_country' => 'country page views',
+        'ad_planner_page_view_country_percent' => 'Country Page View %',
+        'ad_planner_PoC_page_views' => 'PoC page views',
+        'ad_planner_page_view_PoC_percent' => 'PoC page view %',
+    );
+
+ $csv_columns = array(
+        'country_code',
+        'country_name',
+        'total_ips',
+        'total_asns',
+        'points_of_control',
+        'ips_per_points_of_control',
+        'complexity',
+        'ad_planner_country_top_sites',
+        'ad_planner_top_sites_in_country',
+        'ad_planner_top_sites_country_percent',
+        'ad_planner_top_sites_in_PoC',
+        'ad_planner_top_sites_PoC_percent',
+        'ad_planner_total_page_views',
+        'ad_planner_page_views_in_country',
+        'ad_planner_page_view_country_percent',
+        'ad_planner_PoC_page_views',
+        'ad_planner_page_view_PoC_percent',
+    );
+
+
+  $stdout = fopen("php://output", "w+");
+ 
+  foreach ($csv_columns as $column)
+      {
+        $csv_header[] =  $column_headings[$column];
+      }
+
+
+  fputcsv($stdout, $csv_header);
+
+  foreach ($countries_xml as $country) {
+    $info_hash = get_country_info_hash($country);
+
+    $country_info = array();
+    //THIS should really use CLOSURES but UBUNTU doesn't yet have php 5.3
+    foreach ($csv_columns as $column)
+      {
+        $country_info[] = $info_hash[$column];
+      }
+
+    fputcsv($stdout, $country_info);
+  }
+}

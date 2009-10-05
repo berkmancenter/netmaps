407ceb48de37490e172395343e667f838db727a5
David Larochelle <dlarochelle@dlarochelle-laptop.(none)>
2009-10-05 17:28:35 -0400

    Add CSV data file link.

--- india_asn/asn_web_graphs/raw_data.php
+++ india_asn/asn_web_graphs/raw_data.php
@@ -36,6 +36,7 @@ The following xml file contains all of the data presented in these web pages, in
 </ul>
 
 <p> <a href="results/results.xml">Our results in XML format.</a></p>
+<p> <a href="./data_csv.php">Our results in CSV format.</a></p>
 
 <p>For more information about the methodology used to generate this data, see the <a href="methods.php">Methods</a> page.<p>
 

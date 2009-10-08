b1461e90aa78523df0a44feed6a211c811b47627
David Larochelle <dlarochelle@dlarochelle-laptop.(none)>
2009-10-08 18:05:54 -0400

    Make full regeneration actually do a full regeneration.

--- india_asn/full_regeneration.sh
+++ india_asn/full_regeneration.sh
@@ -4,4 +4,4 @@ working_dir=`dirname $0`
 
 cd $working_dir
 
-./update_downloaded_caida_data.sh; ./update_generated_data.sh ; ./update_map.sh 
+./update_downloaded_caida_data.sh; ./update_generated_data.sh ; ./update_map.sh; ./regenerate_map.sh

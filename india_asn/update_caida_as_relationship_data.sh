94ea48568678d39a4aef657a95adc230bc1f9678
David Larochelle <dlarochelle@dlarochelle-laptop.(none)>
2009-10-13 17:17:37 -0400

    Download data from the previous year if there is no data for the current year.

--- india_asn/update_caida_as_relationship_data.sh
+++ india_asn/update_caida_as_relationship_data.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
+set -u
+set -e
 source ./set_common_script_variables.sh
 
 CAIDA_SERVER=as-rank.caida.org
-YEAR=`date +%Y`
+
+function download_for_year
+{
+local YEAR=$1
+
 CAIDA_DATA_DIRECTORY_URL=http://$CAIDA_SERVER/data/$YEAR
 
-wget -P $DATA_DOWNLOAD_DIRECTORY -q -N  -r -l 1 $CAIDA_DATA_DIRECTORY_URL
+wget  -P $DATA_DOWNLOAD_DIRECTORY -q -N  -r -l 1 $CAIDA_DATA_DIRECTORY_URL 
+}
+
+YEAR=`date +%Y`
+download_for_year $YEAR
+find $DATA_DOWNLOAD_DIRECTORY/$CAIDA_SERVER > /dev/null || {
+echo "Trying prior year";
+YEAR=`date --date='1 year ago' +%Y`
+download_for_year $YEAR
+
+}
 find $DATA_DOWNLOAD_DIRECTORY/$CAIDA_SERVER -iname 'as-rel.*txt' -print0 | xargs -0 ls -tdr | tail -n 1 > $DATA_DOWNLOAD_DIRECTORY/newest_as_rel_file_name.txt

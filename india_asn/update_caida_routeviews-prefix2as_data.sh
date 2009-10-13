74a5eee473100335e73a5fadf3f1448f80e4a5c3
David Larochelle <dlarochelle@dlarochelle-laptop.(none)>
2009-10-13 16:13:24 -0400

    Use the current month rather a hard coded date.
    This data seems to be updated daily but there is a possible issue with running this on the first of the month as the CAIDA data may be a few hours behind.

--- india_asn/update_caida_routeviews-prefix2as_data.sh
+++ india_asn/update_caida_routeviews-prefix2as_data.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+#This data seems to be updated daily but there is a possible issue with running this on the first of the month as the CAIDA data may be a few hours behind.
+
 source set_common_script_variables.sh
 
 CAIDA_ROUTEVIEWS_SERVER=data.caida.org
-CAIDA_DATA_ROUTEVIEWS_DIRECTORY=datasets/routing/routeviews-prefix2as/2009/06
+CAIDA_DATA_ROUTEVIEWS_DIRECTORY_BASE=datasets/routing/routeviews-prefix2as
+YEAR=`date +%Y`
+MONTH=`date +%m`
+CAIDA_DATA_ROUTEVIEWS_DIRECTORY=$CAIDA_DATA_ROUTEVIEWS_DIRECTORY_BASE/$YEAR/$MONTH
 CAIDA_DATA_ROUTEVIEWS_DIRECTORY_URL=http://$CAIDA_ROUTEVIEWS_SERVER/$CAIDA_DATA_ROUTEVIEWS_DIRECTORY
-echo $CAIDA_DATA_ROUTEVIEWS_DIRECTORY
+echo $CAIDA_DATA_ROUTEVIEWS_DIRECTORY_URL
 
-wget -P $DATA_DOWNLOAD_DIRECTORY --debug  -N  -A *.gz,*.html -r -l 2 $CAIDA_DATA_ROUTEVIEWS_DIRECTORY_URL --exclude-directories=$CAIDA_DATA_ROUTEVIEWS_DIRECTORY/1,$CAIDA_DATA_ROUTEVIEWS_DIRECTORY/2,$CAIDA_DATA_ROUTEVIEWS_DIRECTORY/3,$CAIDA_DATA_ROUTEVIEWS_DIRECTORY/4,$CAIDA_DATA_ROUTEVIEWS_DIRECTORY/5
+wget -P $DATA_DOWNLOAD_DIRECTORY --debug --no-parent -N  -A *.gz,*.html -r -l 2 --include-directories=$CAIDA_DATA_ROUTEVIEWS_DIRECTORY $CAIDA_DATA_ROUTEVIEWS_DIRECTORY_URL 
 find $DATA_DOWNLOAD_DIRECTORY/$CAIDA_ROUTEVIEWS_SERVER -iname 'routeviews-rv2*.pfx2as.*gz' -print0 | xargs -0 ls -tdr | tail -n 1 > $DATA_DOWNLOAD_DIRECTORY/newest_prefix2as_file_name.txt
 ln -sf `pwd`/`cat  $DATA_DOWNLOAD_DIRECTORY/newest_prefix2as_file_name.txt` $DATA_DOWNLOAD_DIRECTORY/newest_prefix2as_file.txt

81b48b057b6d2e8dd2a86cdcc2618ae472b7cd2d
David Larochelle <dlarochelle@dlarochelle-laptop.(none)>
2009-10-13 16:25:40 -0400

    Download data for the current year.

--- india_asn/update_caida_as_relationship_data.sh
+++ india_asn/update_caida_as_relationship_data.sh
@@ -3,7 +3,8 @@
 source ./set_common_script_variables.sh
 
 CAIDA_SERVER=as-rank.caida.org
-CAIDA_DATA_DIRECTORY_URL=http://$CAIDA_SERVER/data/2009/
+YEAR=`date +%Y`
+CAIDA_DATA_DIRECTORY_URL=http://$CAIDA_SERVER/data/$YEAR
 
 wget -P $DATA_DOWNLOAD_DIRECTORY -q -N  -r -l 1 $CAIDA_DATA_DIRECTORY_URL
 find $DATA_DOWNLOAD_DIRECTORY/$CAIDA_SERVER -iname 'as-rel.*txt' -print0 | xargs -0 ls -tdr | tail -n 1 > $DATA_DOWNLOAD_DIRECTORY/newest_as_rel_file_name.txt

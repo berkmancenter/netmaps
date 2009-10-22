209d4134b47705ca656c74516569767117ae84ab
David Larochelle <dlarochelle@dlarochelle-laptop.(none)>
2009-10-22 17:31:03 -0400

    Exclude small countries from the CSV file.

--- india_asn/asn_web_graphs/data_csv.php
+++ india_asn/asn_web_graphs/data_csv.php
@@ -9,7 +9,7 @@ header("Content-Disposition: attachment; filename=\"internet_mapping_data.csv\""
 
 include "./xml_utils.php";
 
-$countries_xml = get_all_countries();
+$countries_xml = get_unsorted_country_list();
 
 csv_dump($countries_xml);
 

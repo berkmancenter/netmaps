a42deeba958816656bea48d4c8a27d5059c534f5
David Larochelle <dlarochelle@dlarochelle-laptop.(none)>
2009-10-28 13:03:36 -0400

    Add more error checking.

--- india_asn/update_generated_data.sh
+++ india_asn/update_generated_data.sh
@@ -10,17 +10,23 @@ DATABASE_DIRECTORY=db
 mkdir -p $GENERATED_DATA_DIRECTORY
 rm -rf $GENERATED_DATA_DIRECTORY/*
 mkdir -p $GENERATED_DATA_DIRECTORY
+
+#Verify that the files exist
+cat $DATA_DOWNLOAD_DIRECTORY/newest_as_rel_file_name.txt > /dev/null
+cat $DATA_DOWNLOAD_DIRECTORY/newest_as_rel_file_name.txt | xargs cat > /dev/null
+
 cat $DATA_DOWNLOAD_DIRECTORY/newest_as_rel_file_name.txt | xargs cat  | ./generate_asn_to_country_list.pl  >  $GENERATED_DATA_DIRECTORY/asn_to_country.csv
+
 cat $DATA_DOWNLOAD_DIRECTORY/newest_as_rel_file_name.txt | xargs cat | ./generate_asn_cymru_tsv.pl  >  $GENERATED_DATA_DIRECTORY/asn_info.tsv
 rm -rf  $DATABASE_DIRECTORY
 mkdir -p $DATABASE_DIRECTORY
-rm $DATABASE_DIRECTORY/as_info.db
+rm -f $DATABASE_DIRECTORY/as_info.db
 sqlite3 $DATABASE_DIRECTORY/as_info.db < sql_scripts/as_info.sql
-rm $DATABASE_DIRECTORY/asn_ip_counts.db
+rm -f $DATABASE_DIRECTORY/asn_ip_counts.db
 sqlite3 $DATABASE_DIRECTORY/asn_ip_counts.db < sql_scripts/asn_ip_counts.sql
 ./import_ip_prefixes_to_db.pl
-rm $DATABASE_DIRECTORY/asn_taxonomy.db
+rm -f $DATABASE_DIRECTORY/asn_taxonomy.db
 sqlite3 $DATABASE_DIRECTORY/asn_taxonomy.db < sql_scripts/asn_taxonomy.sql
-rm      $DATABASE_DIRECTORY/ad_words.db
+rm -f     $DATABASE_DIRECTORY/ad_words.db
 sqlite3 $DATABASE_DIRECTORY/ad_words.db < sql_scripts/ad_words_schema.sql 
 ./import_ad_planner_data_into_sqlite.sh

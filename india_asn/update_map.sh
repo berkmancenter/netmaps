972c98c1406a74b9e77ba6ed9ebbebc5629dff2c
David Larochelle <dlarochelle@dlarochelle-laptop.(none)>
2011-05-19 10:52:08 -0400

    Let script update the generated data even if we don't need to download a new map.

--- india_asn/update_map.sh
+++ india_asn/update_map.sh
@@ -14,7 +14,7 @@ if ! cmp --quiet $NEWEST_AS_REL_LOCATION_FILE $LAST_AS_REL_LOCATION_FILE
 then  echo "files differ ";
 else
 echo "Files don't differ";
-exit;
+#exit;
 fi
 
 
@@ -26,4 +26,4 @@ mv results $WEB_DIRECTORY/results_new
 rm -rf $WEB_DIRECTORY/results_old
 mv $WEB_DIRECTORY/results $WEB_DIRECTORY/results_old
 mv $WEB_DIRECTORY/results_new  $WEB_DIRECTORY/results
-mv $NEWEST_AS_REL_LOCATION_FILE $LAST_AS_REL_LOCATION_FILE
+#mv $NEWEST_AS_REL_LOCATION_FILE $LAST_AS_REL_LOCATION_FILE

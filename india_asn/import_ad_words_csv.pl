ac772bd60de0cf71fce68243aa51cd0f9a344916
David Larochelle <dlarochelle@dlarochelle-laptop.(none)>
2011-08-16 13:17:42 -0400

    Update script to handle the new google ad words format.

--- india_asn/import_ad_words_csv.pl
+++ india_asn/import_ad_words_csv.pl
@@ -6,6 +6,7 @@ use Class::CSV;
 use DBIx::Simple;
 use Data::Dumper;
 use Locale::Country qw(country2code);
+use Perl6::Say;
 
 sub main
 {
@@ -35,26 +36,68 @@ sub parse_file
 
     #    $dbh->do("insert into query (query_id, query_name,query_date) values (1,'stuff','2006-11-06')");
 
+    my $fields_old_format =    [         qw / site_name category composition_index audience_reach audience_unique_users country_unique_users country_page_views gcn_text gcn_images gcn_videos gcn_gadgets gcn_impressions_per_day / ];
+
+    my $fields_new_format =  [qw / Publisher AdWords_Posting_URL site_name Type category Has_Advertising gcn_text gcn_images Audio gcn_videos Rich_Media Expandable composition_index audience_reach audience_unique_users country_unique_users country_page_views foo / ];
+
+    #say STDERR Dumper( $fields_new_format );
+
+    #say STDERR "Parsing file ";
+
     my $csv = Class::CSV->parse(
         filename => $filename,
-        fields   => [
-            qw / site_name category composition_index audience_reach audience_unique_users country_unique_users country_page_views gcn_text gcn_images gcn_videos gcn_gadgets gcn_impressions_per_day /
-        ]
+        fields   => $fields_new_format
     );
+
+    say STDERR "Parsed File $filename";
+
+    #exit;
 
     my $country_name = lc($filename);
     $country_name =~ s/.*\///;
 
     $country_name =~ s/\sadplanner.csv//i;
 
+    $country_name =~ s/-20110810.csv//i;
+    $country_name =~ s/-20110811.csv//i;
+    $country_name =~ s/-/ /gi;
+
     print "Country Name: '$country_name'\n";
 
     my $country_code = country2code($country_name);
+
+    say "Country Code: $country_code";
+
+    if ( ! $country_code )
+    {
+       say STDERR "Warning couldn't find the country code for $country_name\n";
+    }
 
     shift @{ $csv->lines };
 
     for my $line ( @{ $csv->lines } )
     {
+
+       # add 'fake' fields if necessary
+
+       my $fake_fields = [ qw / gcn_gadgets gcn_impressions_per_day  / ];
+
+       foreach my $fake_field ( @ { $fake_fields } )
+       {
+	  eval { $line->get( $fake_field ) };
+	  if ( $@ )
+	  {
+              #say STDERR " Adding fake field: $fake_field";
+	      $line->_build_fields( [ $fake_field ] );
+	      $line->set( $fake_field => '');
+	  }
+       }
+
+       my $site_name = $line->get( 'site_name' );
+
+       $site_name =~ s/^domain: //;
+
+       $line->set( 'site_name' => $site_name );
 
 #print Dumper ($line->get(qw / site_name category composition_index audience_reach audience_unique_users country_unique_users country_page_views gcn_text gcn_images gcn_videos gcn_gadgets gcn_impressions_per_day/));
 
